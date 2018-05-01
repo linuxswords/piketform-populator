f0e7390cb95bb747c25d3ded0e6f0fef4b121a33
Martin Knoller Stocker <martin.knoller@gmail.com>
2018-05-01 11:42:00 +0200

    Replaced umlaut

--- src/main/java/Data.java
+++ src/main/java/Data.java
@@ -9,14 +9,14 @@ public class Data {
     static {
         COMMUNITIES = new HashMap<String, String>();
         COMMUNITIES.put("adliswil", "Stadt Adliswil\nGesundheit, Umwelt\nZuerichstr. 17\n 8134 Adliswil");
-        COMMUNITIES.put("rueschlikon", "Gemeindeverwaltung Rueschlikon\nGesundheitsbehoerde\nPostfach\n 8803 Rueschlikon");
+        COMMUNITIES.put("rueschlikon", "Gemeindeverwaltung Rüschlikon\nGesundheitsbehörde\nPostfach\n 8803 Rüschlikon");
         COMMUNITIES.put("richterswil", "Gemeinde Richterswil\nAbteilung Gesellschaft\nSeestr. 19\n 8805 Richterswil");
-        COMMUNITIES.put("waedenswil", "Stadt Waedenswil\nSicherheit und Gesundheit\nFlorhofstr. 6\n Postfach\n8820 Waedenswil");
-        COMMUNITIES.put("langnau", "Gemeindeverwaltung\nLangnau am Albis\nGesundheitsbehoerde\n Neue Dorfstr. 14, Postfach\n8135 Langnau am Albis");
-        COMMUNITIES.put("kilchberg", "Gemeindeverwaltung\nKilchberg\nGesundheitsbehoerde\n Alte Landstr. 110\n8802 Kilchberg");
-        COMMUNITIES.put("oberrieden", "Gemeinde\nOberrieden\nAbteilung Bevoelkerung\n Alte Landstr. 32\nPostfach 178\n8942 Oberrieden");
+        COMMUNITIES.put("waedenswil", "Stadt Wädenswil\nSicherheit und Gesundheit\nFlorhofstr. 6\n Postfach\n8820 Wädenswil");
+        COMMUNITIES.put("langnau", "Gemeindeverwaltung\nLangnau am Albis\nGesundheitsbehörde\n Neue Dorfstr. 14, Postfach\n8135 Langnau am Albis");
+        COMMUNITIES.put("kilchberg", "Gemeindeverwaltung\nKilchberg\nGesundheitsbehörde\n Alte Landstr. 110\n8802 Kilchberg");
+        COMMUNITIES.put("oberrieden", "Gemeinde\nOberrieden\nAbteilung Bevölkerung\n Alte Landstr. 32\nPostfach 178\n8942 Oberrieden");
         COMMUNITIES.put("horgen", "Abteilung Alter& Gesundheit\nBaumgaertlistrasse 12\n8810 Horgen");
-        COMMUNITIES.put("hirzel", "Gemeindeverwaltung Hirzel\nGesundheitsbehoerde\nBergstr. 6\n8816 Hirzel");
+        COMMUNITIES.put("hirzel", "Gemeindeverwaltung Hirzel\nGesundheitsbehörde\nBergstr. 6\n8816 Hirzel");
 
 
     }
