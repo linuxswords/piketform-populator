140ac1b042fe2d9378fbfb8b691bd537a778d109
Martin Knoller Stocker <martin.knoller@gmail.com>
2018-05-01 11:38:01 +0200

    Shortened Oberrieden, regenerated pdf files

--- src/main/java/Data.java
+++ src/main/java/Data.java
@@ -14,7 +14,7 @@ public class Data {
         COMMUNITIES.put("waedenswil", "Stadt Waedenswil\nSicherheit und Gesundheit\nFlorhofstr. 6\n Postfach\n8820 Waedenswil");
         COMMUNITIES.put("langnau", "Gemeindeverwaltung\nLangnau am Albis\nGesundheitsbehoerde\n Neue Dorfstr. 14, Postfach\n8135 Langnau am Albis");
         COMMUNITIES.put("kilchberg", "Gemeindeverwaltung\nKilchberg\nGesundheitsbehoerde\n Alte Landstr. 110\n8802 Kilchberg");
-        COMMUNITIES.put("oberrieden", "Gemeindeverwaltung\nOberrieden\nAbteilung Bevoelkerung\n Alte Landstr. 32\nPostfach 178\n8942 Oberrieden");
+        COMMUNITIES.put("oberrieden", "Gemeinde\nOberrieden\nAbteilung Bevoelkerung\n Alte Landstr. 32\nPostfach 178\n8942 Oberrieden");
         COMMUNITIES.put("horgen", "Abteilung Alter& Gesundheit\nBaumgaertlistrasse 12\n8810 Horgen");
         COMMUNITIES.put("hirzel", "Gemeindeverwaltung Hirzel\nGesundheitsbehoerde\nBergstr. 6\n8816 Hirzel");
 
